60f9375ae891bf8d659a19e91330e12548443395
DavidBewley <davidmartinbewley@gmail.com>
2020-11-29 14:59:49 +0000

    Added delete task and buttons for other functions

--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -3,17 +3,30 @@ import './Css/TaskList.css';
 
 class TaskList extends Component {
 
-    createTasks(task) {
-        return <li key={task.key}>{task.text}</li>
-      }
+  constructor(props) {
+    super(props);
+    this.createTasks = this.createTasks.bind(this);
+  }
+  delete(key) {
+    this.props.delete(key);
+  }
 
-    render() {
-        var taskList = this.props.tasks
-        var listTasks = taskList.map(this.createTasks)
-        return (
-            <ul>{listTasks}</ul>
-        );
-    }
+  createTasks(task) {
+    return <li key={task.key}>
+      {task.complete.toString()} - {task.text}
+      <button>Mark As Complete</button>
+      <button>Edit</button>
+      <button onClick={() => this.delete(task.key)}>Delete</button>
+    </li>
+  }
+
+  render() {
+    var taskList = this.props.tasks
+    var listTasks = taskList.map(this.createTasks)
+    return (
+      <ul>{listTasks}</ul>
+    );
+  }
 }
 
 export default TaskList;
--- src/Components/TaskListContainer.js
+++ src/Components/TaskListContainer.js
@@ -1,25 +1,88 @@
 import React, { Component } from "react";
 import './Css/TaskListContainer.css';
-import AddTask from './AddTask.js';
-// import TaskList from './TaskList.js';
+import TaskList from './TaskList.js';
 
 class TaskListContainer extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      tasks: [{
+        key: '1606661929799',
+        text: 'Call Plumber',
+        complete: 'false'
+      }]
+    };
+
+    this.addTask = this.addTask.bind(this);
+    this.deleteTask = this.deleteTask.bind(this);
+
   }
 
-  render(){
-  return (
-    <div className="TaskListContainer">
-      <h1>Todays Tasks</h1>
-      <div className="splitScreen">
-        {/* <div className="leftPane"><TaskList /></div> */}
-        <div className="rightPane"><AddTask /></div>
+  addTask(e) {
+    if (this._inputElement.value !== "") {
+      var newTask = {
+        text: this._inputElement.value,
+        key: Date.now(),
+        complete: false
+      };
+
+      this.setState((prevState) => {
+        return {
+          tasks: prevState.tasks.concat(newTask)
+        };
+      });
+
+      this._inputElement.value = "";
+    }
+    console.log(this.state.tasks);
+
+    e.preventDefault();
+  }
+
+  deleteTask(key){
+    var filteredItems = this.state.tasks.filter(function (item) {
+      return (item.key !== key);
+    });
+    this.setState({
+      tasks: filteredItems
+    });
+  }
+
+  editTask(key, newText){
+    var item = this.state.tasks.filter(function (item) {
+      return (item.key == key);
+    });
+
+    item.text = newText;
+
+    var filteredItems = this.state.tasks.filter(function (item) {
+      return (item.key !== key);
+    });
+    this.setState({
+      tasks: filteredItems
+    });
+  }
+
+  render() {
+    return (
+      <div className="TaskListContainer">
+        <h1>Todays Tasks</h1>
+        <div className="splitScreen">
+          <div className="leftPane"><TaskList tasks={this.state.tasks} delete={this.deleteTask} /></div>
+          <div className="rightPane"><div className="AddTask">
+            <form onSubmit={this.addTask}>
+              <input ref={(a) => this._inputElement = a}
+                placeholder="Task Name">
+              </input>
+              <button type="submit">Add</button>
+            </form>
+          </div>
+          </div>
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default TaskListContainer;
